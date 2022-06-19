24cd11f1b37bff8912196b33b3e81818d2f6572d
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-06-19 14:20:48 +0900

    matigatta

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@ import { TASK_NODE } from "hardhat/builtin-tasks/task-names";
 
 type Hook = (hre: HardhatRuntimeEnvironment) => Promise<void>;
 const hooks = {
+  beforeInitialize: [] as Hook[],
   initialized: [] as Hook[],
 };
-
+export function beforeNetworkInitialize(hook: Hook) {
+  hooks.beforeInitialize.push(hook);
+}
 export function onNetworkInitialized(hook: Hook) {
   hooks.initialized.push(hook);
 }
@@ -16,9 +19,14 @@ task(TASK_NODE)
   .addFlag("ignoreHook", "Ignore the network initialized hook")
   .setAction(async ({ ignoreHook }: { ignoreHook: boolean }, hre, runSuper) => {
     if (!ignoreHook) {
-      for (const initHook of hooks.initialized) {
-        await initHook(hre);
+      for (const hook of hooks.beforeInitialize) {
+        await hook(hre);
       }
     }
     await runSuper();
+    if (!ignoreHook) {
+      for (const hook of hooks.initialized) {
+        await hook(hre);
+      }
+    }
   });
