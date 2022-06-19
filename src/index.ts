532b9dfef4cad9bff9aa1909c7d5f4a089c8a074
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-06-19 14:05:23 +0900

    impl

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,24 @@
-import path from "path";
-import { extendConfig, extendEnvironment } from "hardhat/config";
-import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import { TASK_NODE } from "hardhat/builtin-tasks/task-names";
 
-import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
-// This import is needed to let the TypeScript compiler know that it should include your type
-// extensions in your npm package's types file.
-import "./type-extensions";
+type Hook = (hre: HardhatRuntimeEnvironment) => Promise<void>;
+const hooks = {
+  initialized: [] as Hook[],
+};
 
-extendConfig(
-  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
-    // We apply our default config here. Any other kind of config resolution
-    // or normalization should be placed here.
-    //
-    // `config` is the resolved config, which will be used during runtime and
-    // you should modify.
-    // `userConfig` is the config as provided by the user. You should not modify
-    // it.
-    //
-    // If you extended the `HardhatConfig` type, you need to make sure that
-    // executing this function ensures that the `config` object is in a valid
-    // state for its type, including its extensions. For example, you may
-    // need to apply a default value, like in this example.
-    const userPath = userConfig.paths?.newPath;
+export function onNetworkInitialized(hook: Hook) {
+  hooks.initialized.push(hook);
+}
 
-    let newPath: string;
-    if (userPath === undefined) {
-      newPath = path.join(config.paths.root, "newPath");
-    } else {
-      if (path.isAbsolute(userPath)) {
-        newPath = userPath;
-      } else {
-        // We resolve relative paths starting from the project's root.
-        // Please keep this convention to avoid confusion.
-        newPath = path.normalize(path.join(config.paths.root, userPath));
+task(TASK_NODE)
+  .addFlag("ignoreHook", "Ignore the network initialized hook")
+  .setAction(async ({ ignoreHook }: { ignoreHook: boolean }, hre, runSuper) => {
+    if (!ignoreHook) {
+      for (const initHook of hooks.initialized) {
+        await initHook(hre);
       }
     }
-
-    config.paths.newPath = newPath;
-  }
-);
-
-extendEnvironment((hre) => {
-  // We add a field to the Hardhat Runtime Environment here.
-  // We use lazyObject to avoid initializing things until they are actually
-  // needed.
-  hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField());
-});
+    await runSuper();
+  });
